57e3c1c3ecc015b3c5c8c42995cf87c4da9ec833
Hector Bennett <hectorbennett@outlook.com>
2023-06-05 16:33:34 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import init, { greet } from "wasm";
+
+import reactLogo from "./assets/react.svg";
+import rustWasm from "./assets/rust-wasm.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://rustwasm.github.io/" target="_blank" rel="noreferrer">
+          <img src={rustWasm} className="logo rust-wasm" alt="Rust/Wasm logo" />
+        </a>
+      </div>
+      <h1>Vite + React + TypeScript + Rust/WebAssembly</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            init().then(() => {
+              console.log("init wasm-pack");
+              greet();
+            });
+          }}
+        >
+          Click me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
